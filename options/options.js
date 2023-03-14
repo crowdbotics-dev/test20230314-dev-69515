8b533ff87209bb722d3372e0ddab5468f05f251a
Daniel Palacios <daniel80380@gmail.com>
2023-03-14 12:23:49 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,51 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 13
+  },
+  hr: {
+    marginTop: 20,
+    marginBottom: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  text: {
+    color: "black",
+    fontSize: 20
+  },
+  boldText: {
+    fontSize: 24,
+    backgroundColor: "rgba(0,0,0, 0.1)",
+    marginTop: 10,
+    color: "rgba(0,0,0, 0.6)"
+  },
+  buttonPressed: {
+    backgroundColor: "aquamarine"
+  },
+  buttonNotPressed: {
+    backgroundColor: "blue"
+  },
+  button: {
+    borderRadius: 4,
+    padding: 15,
+    marginTop: 10
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16
+  }
+});
+import { StyleSheet } from "react-native";
 export const globalOptions = {
-  name: "test20230314-dev-69515",
-  url: "https://test20230314-dev-69515.botics.co",
-  api: "https://test20230314-dev-69515.botics.co/api/v1"
+  "name": "test20230314-dev-69515",
+  "url": "https://test20230314-dev-69515.botics.co",
+  "api": "https://test20230314-dev-69515.botics.co/api/v1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "title": "AppEditted",
+    "copy": "Routes available33",
+    "styles": styles
   }
 };
